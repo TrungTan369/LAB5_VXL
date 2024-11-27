c1b1becd0f8f0164971106aa4f815c21c138120e
TrungTan <ttqpentakill@gmail.com>
2024-11-27 16:50:52 +0700

    10%

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_DEBUG_Pin GPIO_PIN_5
+#define LED_DEBUG_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
